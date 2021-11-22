eb1cf8aac50132f3ad95520d6bb1e388c13d61ed
Vanshaj Gupta <vanshaj444@gmail.com>
2021-11-22 13:24:59 +0530

    adding sign-in and sign-up pages

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,22 @@
 module.exports.profile=function(req,res){
     res.end('<h1>user profile</h1>')
 }
+
+module.exports.signUp=function(req,res){
+    return res.render('user_sign_up',{
+        title: 'codeial | sign up'
+    })
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: 'codeial | sign in'
+    })
+}
+
+module.exports.create=function(req,res){
+
+}
+module.exports.createSession=function(req,res){
+    
+}
