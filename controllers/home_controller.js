75be90592665e020f5406c358b9ed89c260575f9
Vanshaj Gupta <vanshaj444@gmail.com>
2021-11-24 14:57:33 +0530

    showing comments and updating also

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -4,7 +4,13 @@ const Post=require('../models/post');
 
 
 module.exports.home=function(req,res){
-    Post.find({}).populate('user').exec(function(err,posts){
+    Post.find({}).populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    }).exec(function(err,posts){
         return res.render('home',{
             title : "home",
             posts : posts
